0ccd4e51ec28b53cbc4e5b4234d93d31cb212b89
W4lly63 <giulfy@hotmail.com>
2016-11-27 12:34:02 +0100

    updates 0.5

---
+++ CfgAmbSounds.hpp
@@ -0,0 +1,171 @@
+
+	class cr0 {
+		name = "cr0";
+		sound[] = {"sounds\cr0.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr15 {
+		name = "cr15";
+		sound[] = {"sounds\cr15.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr1 {
+		name = "cr1";
+		sound[] = {"sounds\cr1.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr16 {
+		name = "cr16";
+		sound[] = {"sounds\cr16.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr2 {
+		name = "cr2";
+		sound[] = {"sounds\cr2.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr18 {
+		name = "cr18";
+		sound[] = {"sounds\cr18.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr3 {
+		name = "cr3";
+		sound[] = {"sounds\cr3.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr17 {
+		name = "cr17";
+		sound[] = {"sounds\cr17.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr4 {
+		name = "cr4";
+		sound[] = {"sounds\cr4.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr19 {
+		name = "cr19";
+		sound[] = {"sounds\cr19.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr5 {
+		name = "cr5";
+		sound[] = {"sounds\cr5.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr20 {
+		name = "cr20";
+		sound[] = {"sounds\cr20.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr6 {
+		name = "cr6";
+		sound[] = {"sounds\cr6.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr21 {
+		name = "cr21";
+		sound[] = {"sounds\cr21.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr7 {
+		name = "cr7";
+		sound[] = {"sounds\cr7.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr22 {
+		name = "cr22";
+		sound[] = {"sounds\cr22.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr8 {
+		name = "cr8";
+		sound[] = {"sounds\cr8.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr23 {
+		name = "cr23";
+		sound[] = {"sounds\cr23.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr9 {
+		name = "cr9";
+		sound[] = {"sounds\cr9.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr24 {
+		name = "cr24";
+		sound[] = {"sounds\cr24.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr10 {
+		name = "cr10";
+		sound[] = {"sounds\cr10.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr25 {
+		name = "cr25";
+		sound[] = {"sounds\cr25.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr11 {
+		name = "cr11";
+		sound[] = {"sounds\cr11.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr26 {
+		name = "cr26";
+		sound[] = {"sounds\cr26.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class cr12 {
+		name = "cr12";
+		sound[] = {"sounds\cr12.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr27 {
+		name = "cr27";
+		sound[] = {"sounds\cr27.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr13 {
+		name = "cr13";
+		sound[] = {"sounds\cr13.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr28 {
+		name = "cr28";
+		sound[] = {"sounds\cr28.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr14 {
+		name = "cr14";
+		sound[] = {"sounds\cr14.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr29 {
+		name = "cr29";
+		sound[] = {"sounds\cr29.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr30 {
+		name = "cr30";
+		sound[] = {"sounds\cr30.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr31 {
+		name = "cr31";
+		sound[] = {"sounds\cr31.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr32 {
+		name = "cr32";
+		sound[] = {"sounds\cr32.ogg", 1, 1};
+		titles[] = {};
+	};
+	class cr33 {
+		name = "cr33";
+		sound[] = {"sounds\cr33.ogg", 1, 1};
+		titles[] = {};
+	};
---
+++ CfgWindSounds.hpp
@@ -0,0 +1,111 @@
+
+	class wn0 {
+		name = "wn0";
+		sound[] = {"sounds\wn0.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl0 {
+		name = "wnl0";
+		sound[] = {"sounds\wn0.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn1 {
+		name = "wn1";
+		sound[] = {"sounds\wn1.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl1 {
+		name = "wnl1";
+		sound[] = {"sounds\wn1.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn2 {
+		name = "wn2";
+		sound[] = {"sounds\wn2.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl2 {
+		name = "wnl2";
+		sound[] = {"sounds\wn2.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn3 {
+		name = "wn3";
+		sound[] = {"sounds\wn3.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl3 {
+		name = "wnl3";
+		sound[] = {"sounds\wn3.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn4 {
+		name = "wn4";
+		sound[] = {"sounds\wn4.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl4 {
+		name = "wnl4";
+		sound[] = {"sounds\wn4.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn5 {
+		name = "wn5";
+		sound[] = {"sounds\wn5.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl5 {
+		name = "wnl5";
+		sound[] = {"sounds\wn5.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn6 {
+		name = "wn6";
+		sound[] = {"sounds\wn6.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl6 {
+		name = "wnl6";
+		sound[] = {"sounds\wn6.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn7 {
+		name = "wn7";
+		sound[] = {"sounds\wn7.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl7 {
+		name = "wnl7";
+		sound[] = {"sounds\wn7.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn8 {
+		name = "wn8";
+		sound[] = {"sounds\wn8.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl8 {
+		name = "wnl8";
+		sound[] = {"sounds\wn8.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn9 {
+		name = "wn9";
+		sound[] = {"sounds\wn9.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl9 {
+		name = "wnl9";
+		sound[] = {"sounds\wn9.ogg", 0.2, 1};
+		titles[] = {};
+	};
+	class wn10 {
+		name = "wn10";
+		sound[] = {"sounds\wn10.ogg", 1, 1};
+		titles[] = {};
+	};
+	class wnl10 {
+		name = "wnl10";
+		sound[] = {"sounds\wn10.ogg", 0.2, 1};
+		titles[] = {};
+	};
