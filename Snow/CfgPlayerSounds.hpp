6e047ac369c13a507d61321bdecd6bf297ace94e
W4lly63 <giulfy@hotmail.com>
2016-12-07 09:36:18 +0100

    Update CfgPlayerSounds.hpp

--- Snow/CfgPlayerSounds.hpp
+++ Snow/CfgPlayerSounds.hpp
@@ -13,6 +13,16 @@
 		sound[] = {"sounds\fs2L.ogg", 1, 1};
 		titles[] = {};
 	};
+	class fs3L {
+		name = "fs3L";
+		sound[] = {"sounds\fs3L.ogg", 1, 1};
+		titles[] = {};
+	};
+	class fs4L {
+		name = "fs4L";
+		sound[] = {"sounds\fs4L.ogg", 1, 1};
+		titles[] = {};
+	};
 	class fs0R {
 		name = "fs0R";
 		sound[] = {"sounds\fs0R.ogg", 1, 1};
@@ -26,5 +36,15 @@
 	class fs2R {
 		name = "fs2R";
 		sound[] = {"sounds\fs2R.ogg", 1, 1};
+		titles[] = {};
+	};
+	class fs3R {
+		name = "fs3R";
+		sound[] = {"sounds\fs3R.ogg", 1, 1};
+		titles[] = {};
+	};
+	class fs4R {
+		name = "fs4R";
+		sound[] = {"sounds\fs4R.ogg", 1, 1};
 		titles[] = {};
 	};
